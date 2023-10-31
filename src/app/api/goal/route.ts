c2401c8d2f8a357fa73a83a7631cd733b26a0e81
Matheusz Nied <matheustae@hotmail.com>
2023-10-30 23:49:10 -0300

    fix: clear codes

--- src/app/api/goal/route.ts
+++ src/app/api/goal/route.ts
@@ -2,15 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
 import { prismaClient } from '@/lib/prisma';
-import { getSession } from 'next-auth/react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { Readable } from 'stream';
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
@@ -20,7 +14,7 @@ export  async function POST(req: NextApiRequest, res: NextApiResponse) {
     const body  = await new Response(req.body).text();
     const {name,category, preview_date} = JSON.parse(body);
     if (!session?.user?.email) {
-      
+      console.log({ error:"Erro"})
       return NextResponse.json({ error:"Erro"});
     }
     const result = await prismaClient.goal.create({
