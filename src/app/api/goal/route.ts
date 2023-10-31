60de04e1bfb79790e09bcfe554717e1862e693c7
Matheusz Nied <matheustae@hotmail.com>
2023-10-31 16:27:22 -0300

    fix: change type NextApiRequest to NextRequest

--- src/app/api/goal/route.ts
+++ src/app/api/goal/route.ts
@@ -1,32 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-import { prismaClient } from '@/lib/prisma';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
+import { prismaClient } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export  async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(
+  req: NextRequest,
+  res: NextApiResponse,
+) {
+  const session = await getServerSession(authOptions);
+  const body = await new Response(req.body).text();
+  const { name, category, preview_date } = JSON.parse(body);
+  if (!session?.user?.email) {
+    console.log({ error: "Erro" });
+    return NextResponse.json({ error: "Erro" });
+  }
+  const result = await prismaClient.goal.create({
+    data: {
+      name: name,
+      category: category,
+      preview_date: preview_date,
+      user: { connect: { email: session?.user?.email } },
+    },
+  });
 
-    const session = await getServerSession(authOptions);
-    const body  = await new Response(req.body).text();
-    const {name,category, preview_date} = JSON.parse(body);
-    if (!session?.user?.email) {
-      console.log({ error:"Erro"})
-      return NextResponse.json({ error:"Erro"});
-    }
-    const result = await prismaClient.goal.create({
-      data: {
-          name: name,
-          category: category,
-          preview_date:preview_date,
-        user: { connect: { email: session?.user?.email } },
-      },
-    });
-  
-    return NextResponse.json(result);  
-
- 
+  return NextResponse.json(result);
 }
